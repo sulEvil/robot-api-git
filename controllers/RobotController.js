720a42a28f6ca034827ed9a593848a41ccbd766e
gals <sultan_galimov@bk.ru>
2023-05-24 10:44:15 +0500

    catches

--- controllers/RobotController.js
+++ controllers/RobotController.js
@@ -2,16 +2,21 @@ import {Robot} from "../models/model.js";
 
 export class RobotController {
     async create(req, res) {
-        const {name, desc, deviceId, userId} = req.body
-        if(!deviceId){
-            return res.json('Error, incorrect data')
+        try {
+            const {name, desc, deviceId, userId} = req.body
+            if(!deviceId){
+                return res.json('Error, incorrect data')
+            }
+            const candidate = await Robot.findOne({where: {deviceId}})
+            if(candidate) {
+                return res.status(201).json(candidate)
+            }
+            const type = await Robot.create({deviceId, userId})
+            return res.status(200).json(type)
+        } catch(e){
+            return res.status(404).json('Непредвиденная ошибка')
         }
-        const candidate = await Robot.findOne({where: {deviceId}})
-        if(candidate) {
-            return res.status(201).json(candidate)
-        }
-        const type = await Robot.create({deviceId, userId})
-        return res.status(200).json(type)
+       
     }
     async getAll(req, res) {
         const types = await Robot.findAll()
