3eb54110f3b010c7b23afdd46dc4511f57262f9b
gals <sultan_galimov@bk.ru>
2023-05-24 10:30:04 +0500

    statuses

--- controllers/RobotController.js
+++ controllers/RobotController.js
@@ -4,12 +4,12 @@ export class RobotController {
     async create(req, res) {
         const {name, desc, deviceId, userId} = req.body
         if(!deviceId){
-            return res.status(400).json('Error, incorrect data')
+            return res.json('Error, incorrect data')
         }
         const candidate = await Robot.findOne({where: {deviceId}})
         if(candidate) {
             res.send('Robot was created')
-            return res.status(200).json(candidate)
+            return res.json(candidate)
         }
         const type = await Robot.create({deviceId, userId})
         res.send('Robot have been created')
